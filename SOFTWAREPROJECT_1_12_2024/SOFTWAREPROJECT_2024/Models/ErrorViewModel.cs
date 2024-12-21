068bce46dbe5620da4a57cd92772f3996aad2f1d
Fevzi Güler <fvz_glr@hotmail.com>
2024-12-21 14:56:26 +0300

    FEVZİ SON GÜNCELLEME

--- SOFTWAREPROJECT_1_12_2024/SOFTWAREPROJECT_2024/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SOFTWAREPROJECT_2024.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
